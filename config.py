cee5861f2fb31a4ca9e9ea7f0aabd37b9822911e
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-04-26 01:19:09 +0530

    Working with database.Config file updated for db paths and migrations.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 WTF_CSRF_ENABLED=True
 SECRET_KEY = 'robin101!'
 
@@ -7,4 +9,9 @@ OPENID_PROVIDERS = [
                     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
                         {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
                             {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQL_ALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')  #this path is our database file
+SQL_ALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repository')   #this is the folder where i will store SQLAlchemy-migrate data files
 
