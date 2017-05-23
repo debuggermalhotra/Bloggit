526520319086725036416f32b3cc3a6f087f9eea
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-05-23 23:07:22 +0530

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import basedir
 
 app=Flask(__name__)
 app.config.from_object('config')  #telling flask to read and use config.py
 db = SQLAlchemy(app)
+
+lm=LoginManager()
+lm.init_app(app)
+old=OpenId(app, os.path.join(basedir,'tmp'))
 
 from app import views, models
 
