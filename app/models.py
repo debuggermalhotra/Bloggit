3d09dbf02b0d2ae6e73aee7b523706f48c6101e4
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-07-05 13:01:38 +0530

    commit

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts=db.relationship('Post',backref='author',lazy='dynamic')
 
+
     def __repr__(self): #__repr__ tells python how to print objects of this class
         return '<User %r>' % (self.nickname)
 
