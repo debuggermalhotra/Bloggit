c456359c02543ed385bafd5e48a90b5ed6a4d9cc
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-08-05 16:22:57 +0530

    Left with OpenID templating

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_openid import basedir
+from flask_openid import OpenID
+from config import basedir
 
 app=Flask(__name__)
 app.config.from_object('config')  #telling flask to read and use config.py
@@ -10,7 +11,7 @@ db = SQLAlchemy(app)
 
 lm=LoginManager()
 lm.init_app(app)
-old=OpenId(app, os.path.join(basedir,'tmp'))
+lm.login_view='login'
+oid=OpenId(app, os.path.join(basedir,'tmp'))  #flask-openid needs a apth to a temp folder where files can be stored
 
 from app import views, models
-
--- app/views.py
+++ app/views.py
@@ -6,9 +6,10 @@ from .models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user={"nickname":"chetan"} #random user
-    posts=[#fake array of posts
+    user=g.user
+    posts=[
             {
                 'author':{'nickname':'Trickyman'},
                 'body': 'In Delhi, it feels like the summer of 69....more like 69 degrees! :p
@@ -24,6 +25,13 @@ def index():
                            user=user,
                            posts=posts)
 
+
+# function that loads a user from the database
+@lm.user_loader  #this decorator is used to register this func. with Flask-Login
+def load_user(id):
+    return User.query.get(int(id))
+
+
 @app.route('/login',methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
@@ -35,7 +43,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
         session['remember_me']=form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname','email']) 
+        return oid.try_login(form.openid.data, ask_for=['nickname','email'])
 
         return redirect('/index')
     return render_template('login.html',
@@ -43,7 +51,30 @@ def login():
                            form = form,
                            providers=app.config['OPENID_PROVIDERS'])
 
-# function that loads a user from the database
-@lm.user_loader  #this decorator is used to register this func. with Flask-Login
-def load_user(id):
-    return User.query.get(int(id))
+
+#since this function is decorated with befor_request, it will run before the view function each time a request is received
+@app.befor_request
+def befor_request():
+    g.user=current_user #current_user global is set by Flask-Login
+
+@oid.after_login
+def after_login(resp): #resp contains the response/info. returned by OpenID provider
+    #validating if email is provided or not
+    if resp.email is None or resp.email == "":
+        flash("Invalid login!! Try again.")
+        return redirect(url_for('login'))
+    #searching our database for the email
+    user=User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname=resp.nickname
+        if nickname is None or nickname="":
+            nickname=User(nickname=nickname,email=resp.email)
+            db.session.add(user)
+            db.session.commit()
+    remember_me=False
+    #loading remember_me from flask session
+    if 'remember_me' in session:
+        remember_me=session['remember_me']
+        session.pop('remember_me', None)
+        login_user(user, remember=remember_me) #registering this is a valid login
+        return redirect(request.args.get('next') or url_for('index'))
